b44bdace4d6b48d39dae4207578aac3a6a20e1db
r.k <magmaslime@gmail.com>
2014-04-25 12:23:17 +0900

    no message

---
+++ task/rk_hello.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by r.k on 2014/04/25.
+//  Copyright (c) 2014年 r.k. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
